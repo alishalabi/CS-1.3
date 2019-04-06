f6b1ee93e142b0f2e19e5ee1471c59d85d8b3f5c
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-04-06 14:07:39 -0700

    Working on binary search, have issues

--- recursion/search.py
+++ recursion/search.py
@@ -19,21 +19,12 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # implement linear search recursively here
-    # if index < (len(array) - 1):
-    #     if array[index] == item:
-    #         return item
-    #     else:
-    #         index += 1
-    # else:
-    #     return None
-    # End scenario: index has gone past length of array - not found
-    if index > len(array) - 1:
-        return "Bop"
-        # return None
-    elif array[index] == item:
+    if array[index] == item:
         return index
+    elif index < (len(array) - 1):
+        return linear_search_recursive(array, item, (index + 1))
     else:
-        linear_search_recursive(array, item, (index + 1))
+        return None
 
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
@@ -43,23 +34,70 @@ def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
+    # return binary_search_iterative(array, item)
+    return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
+    # Define initial array that will eventually be sliced
+    chunk = array
+    # Scenario: item not found, chunk can still be searched
+    while len(chunk) > 0:
+        half_mark_index = int(len(chunk) / 2)
+        if array.index(item) == half_mark_index:
+            return array.index(item)
+        elif array.index(item) < half_mark_index:
+            chunk = chunk[:half_mark_index]
+        else:
+            chunk = chunk[half_mark_index:]
+    return None
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
 
+# names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie']
+
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    pass
+    # implement binary search recursively here
+    # Slice array based on left/right
+    if right is not None:
+        chunk = array[:right]
+    elif left is not None:
+        chunk = array[left:]
+    # Exit Scenario: array can no longer be searched, item not in array
+    if len(chunk) == 0:
+        return None
+    half_mark_index = int(len(chunk) / 2)
+    """
+    Expected 1:
+    Expected 2:
+    """
+    # Exit Scenario: item found
+    if chunk.index(item) == half_mark_index:
+        return array.index(item)
+    """
+    Expected 1:
+    Expected 2:
+    """
+
+    # Recursion Scenario: item occurs in first half of array
+    elif chunk.index(item) < half_mark_index:
+        return binary_search_recursive(array, item, None, half_mark_index)
+    """
+    Expected 1:
+    Expected 2:
+    """
+    else:
+        # elif chunk.index(item) < half_mark_index:
+        return binary_search_recursive(array, item, half_mark_index, None)
+    """
+    Expected 1:
+    Expected 2:
+    """
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
 
 
-names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie']
-print(linear_search(names, "Brian"))
+# names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie']
+# print(linear_search(names, "Brian"))
