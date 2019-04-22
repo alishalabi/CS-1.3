47e025dd760d8ee0ddf43c395983b1db00040d1c
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-04-22 14:51:15 -0700

    Fixed hashtable architecture

--- hashtables/hashtable.py
+++ hashtables/hashtable.py
@@ -1,146 +1,161 @@
-!python
-
-from hashtable import HashTable
-import unittest
-# Python 2 and 3 compatibility: unittest module renamed this assertion method
-if not hasattr(unittest.TestCase, 'assertCountEqual'):
-    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+#!python
+import sys
+sys.path.append("../")
+from linkedlists.linkedlist import LinkedList
 
 
-class HashTableTest(unittest.TestCase):
+class HashTable(object):
 
-    def test_init(self):
-        ht = HashTable(4)
-        assert len(ht.buckets) == 4
-        assert ht.length() == 0
-        assert ht.size == 0
+    def __init__(self, init_size=8):
+        """Initialize this hash table with the given initial size."""
+        self.buckets = [LinkedList() for i in range(init_size)]
+        self.size = 0  # Number of key-value entries
 
-    def test_keys(self):
-        ht = HashTable()
-        assert ht.keys() == []
-        ht.set('I', 1)
-        assert ht.keys() == ['I']
-        ht.set('V', 5)
-        self.assertCountEqual(ht.keys(), ['I', 'V'])  # Ignore item order
-        ht.set('X', 10)
-        self.assertCountEqual(ht.keys(), ['I', 'V', 'X'])  # Ignore item order
+    def __str__(self):
+        """Return a formatted string representation of this hash table."""
+        items = ['{!r}: {!r}'.format(key, val) for key, val in self.items()]
+        return '{' + ', '.join(items) + '}'
 
-    def test_values(self):
-        ht = HashTable()
-        assert ht.values() == []
-        ht.set('I', 1)
-        assert ht.values() == [1]
-        ht.set('V', 5)
-        self.assertCountEqual(ht.values(), [1, 5])  # Ignore item order
-        ht.set('X', 10)
-        self.assertCountEqual(ht.values(), [1, 5, 10])  # Ignore item order
+    def __repr__(self):
+        """Return a string representation of this hash table."""
+        return 'HashTable({!r})'.format(self.items())
 
-    def test_items(self):
-        ht = HashTable()
-        assert ht.items() == []
-        ht.set('I', 1)
-        assert ht.items() == [('I', 1)]
-        ht.set('V', 5)
-        self.assertCountEqual(ht.items(), [('I', 1), ('V', 5)])
-        ht.set('X', 10)
-        self.assertCountEqual(ht.items(), [('I', 1), ('V', 5), ('X', 10)])
+    def _bucket_index(self, key):
+        """Return the bucket index where the given key would be stored."""
+        return hash(key) % len(self.buckets)
 
-    def test_length(self):
-        ht = HashTable()
-        assert ht.length() == 0
-        ht.set('I', 1)
-        assert ht.length() == 1
-        ht.set('V', 5)
-        assert ht.length() == 2
-        ht.set('X', 10)
-        assert ht.length() == 3
+    def load_factor(self):
+        """Return the load factor, the ratio of number of entries to buckets.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+        # Calculate load factor
+        return self.size / len(self.buckets)
 
-    def test_size(self):
-        ht = HashTable()
-        assert ht.size == 0
-        ht.set('I', 1)
-        assert ht.size == 1
-        ht.set('V', 5)
-        assert ht.size == 2
-        ht.set('X', 10)
-        assert ht.size == 3
+    def keys(self):
+        """Return a list of all keys in this hash table.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+        # Collect all keys in each of the buckets
+        all_keys = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_keys.append(key)
+        return all_keys
 
-    def test_resize(self):
-        ht = HashTable(2)  # Set init_size to 2
-        assert ht.size == 0
-        assert len(ht.buckets) == 2
-        assert ht.load_factor() == 0
-        ht.set('I', 1)
-        assert ht.size == 1
-        assert len(ht.buckets) == 2
-        assert ht.load_factor() == 0.5
-        ht.set('V', 5)  # Should trigger resize
-        assert ht.size == 2
-        assert len(ht.buckets) == 4
-        assert ht.load_factor() == 0.5
-        ht.set('X', 10)
-        assert ht.size == 3
-        assert len(ht.buckets) == 4
-        assert ht.load_factor() == 0.75
-        ht.set('L', 50)  # Should trigger resize
-        assert ht.size == 4
-        assert len(ht.buckets) == 8
-        assert ht.load_factor() == 0.5
+    def values(self):
+        """Return a list of all values in this hash table.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+        # Collect all values in each of the buckets
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
 
-    def test_contains(self):
-        ht = HashTable()
-        ht.set('I', 1)
-        ht.set('V', 5)
-        ht.set('X', 10)
-        assert ht.contains('I') is True
-        assert ht.contains('V') is True
-        assert ht.contains('X') is True
-        assert ht.contains('A') is False
+    def items(self):
+        """Return a list of all entries (key-value pairs) in this hash table.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+        # Collect all pairs of key-value entries in each of the buckets
+        all_items = []
+        for bucket in self.buckets:
+            all_items.extend(bucket.items())
+        return all_items
 
-    def test_set_and_get(self):
-        ht = HashTable()
-        ht.set('I', 1)
-        ht.set('V', 5)
-        ht.set('X', 10)
-        assert ht.get('I') == 1
-        assert ht.get('V') == 5
-        assert ht.get('X') == 10
-        assert ht.length() == 3
-        assert ht.size == 3
-        with self.assertRaises(KeyError):
-            ht.get('A')  # Key does not exist
+    def length(self):
+        """Return the number of key-value entries by traversing its buckets.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+        # Count number of key-value entries in each of the buckets
+        item_count = 0
+        for bucket in self.buckets:
+            item_count += bucket.length()
+        return item_count
+        # Equivalent to this list comprehension:
+        return sum(bucket.length() for bucket in self.buckets)
 
-    def test_set_twice_and_get(self):
-        ht = HashTable()
-        ht.set('I', 1)
-        ht.set('V', 4)
-        ht.set('X', 9)
-        assert ht.length() == 3
-        assert ht.size == 3
-        ht.set('V', 5)  # Update value
-        ht.set('X', 10)  # Update value
-        assert ht.get('I') == 1
-        assert ht.get('V') == 5
-        assert ht.get('X') == 10
-        assert ht.length() == 3  # Check length is not overcounting
-        assert ht.size == 3  # Check size is not overcounting
+    def contains(self, key):
+        """Return True if this hash table contains the given key, or False.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Check if an entry with the given key exists in that bucket
+        entry = bucket.find(lambda key_value: key_value[0] == key)
+        return entry is not None  # True or False
 
-    def test_delete(self):
-        ht = HashTable()
-        ht.set('I', 1)
-        ht.set('V', 5)
-        ht.set('X', 10)
-        assert ht.length() == 3
-        assert ht.size == 3
-        ht.delete('I')
-        ht.delete('X')
-        assert ht.length() == 1
-        assert ht.size == 1
-        with self.assertRaises(KeyError):
-            ht.delete('X')  # Key no longer exists
-        with self.assertRaises(KeyError):
-            ht.delete('A')  # Key does not exist
+    def get(self, key):
+        """Return the value associated with the given key, or raise KeyError.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        entry = bucket.find(lambda key_value: key_value[0] == key)
+        if entry is not None:  # Found
+            # Return the given key's associated value
+            assert isinstance(entry, tuple)
+            assert len(entry) == 2
+            return entry[1]
+        else:  # Not found
+            raise KeyError('Key not found: {}'.format(key))
 
+    def set(self, key, value):
+        """Insert or update the given key with its associated value.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        # Check if an entry with the given key exists in that bucket
+        entry = bucket.find(lambda key_value: key_value[0] == key)
+        if entry is not None:  # Found
+            # In this case, the given key's value is being updated
+            # Remove the old key-value entry from the bucket first
+            bucket.delete(entry)
+        # Case: entry is not found, increase size of our hash table
+        else:
+            self.size += 1
+        # Insert the new key-value entry into the bucket in either case
+        bucket.append((key, value))
+        # Check if the load factor exceeds a threshold such as 0.75
+        if self.load_factor() > 0.75:
+            # If so, automatically resize to reduce the load factor
+            self._resize()
 
-if __name__ == '__main__':
-    unittest.main()
+    def delete(self, key):
+        """Delete the given key and its associated value, or raise KeyError.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        entry = bucket.find(lambda key_value: key_value[0] == key)
+        if entry is not None:  # Found
+            # Remove the key-value entry from the bucket
+            bucket.delete(entry)
+        else:  # Not found
+            raise KeyError('Key not found: {}'.format(key))
+
+    def _resize(self, new_size=None):
+        """Resize this hash table's buckets and rehash all key-value entries.
+        Should be called automatically when load factor exceeds a threshold
+        such as 0.75 after an insertion (when set is called with a new key).
+        Best and worst case running time: ??? under what conditions? [TODO]
+        Best and worst case space usage: ??? what uses this memory? [TODO]"""
+        # If unspecified, choose new size dynamically based on current size
+        if new_size is None:
+            new_size = len(self.buckets) * 2  # Double size
+        # Option to reduce size if buckets are sparsely filled (low load factor)
+        elif new_size is 0:
+            new_size = len(self.buckets) / 2  # Half size
+        # Get a list to temporarily hold all current key-value entries
+        all_items = self.items()
+        # Reset size property to 0, will be re-tallied in the set method
+        self.size = 0
+        # Create a new list of new_size total empty linked list buckets
+        self.buckets = [LinkedList() for i in range(init_size)]
+        # Insert each key-value entry into the new list of buckets,
+        # which will rehash them into a new bucket index based on the new size
+        for item in all_items:
+            self.set(item[0], item[1])
